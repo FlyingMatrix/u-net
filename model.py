9851038cb7b6fb9e1e717608ce3521154419a578
FlyingMatrix <jxzhang.acm@gmail.com>
2026-01-11 02:39:31 +0100

    U-net model

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as F
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x): 
+        return self.conv(x)
+    

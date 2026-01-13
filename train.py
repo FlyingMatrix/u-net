cac221fa279052963f771ce837ee7823c85306e2
FlyingMatrix <jxzhang.acm@gmail.com>
2026-01-13 11:39:51 +0100

    update train.py

--- train.py
+++ train.py
@@ -13,4 +13,18 @@ from utils import (
     save_predictions_as_imgs,
 )
 
+# Hyperparameters
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 5
+NUM_EPOCHS = 10
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 512
+IMAGE_WIDTH = 512
+PIN_MEMORY = True # page-locked memory, which allows faster and asynchronous transfers from CPU to GPU
+LOAD_MODEL = False
+TRAIN_IMG_DIR = "data/train/imgs"
+TRAIN_MASK_DIR = "data/train/labels"
+VAL_IMG_DIR = "data/test/imgs"
+VAL_IMG_DIR = "data/test/imgs"
 

5bd8cabdcb3efa3a83cbdacd0eb8f39037518bc5
FlyingMatrix <jxzhang.acm@gmail.com>
2026-01-13 13:01:13 +0100

    update train.py

--- train.py
+++ train.py
@@ -21,10 +21,54 @@ NUM_EPOCHS = 10
 NUM_WORKERS = 2
 IMAGE_HEIGHT = 512
 IMAGE_WIDTH = 512
-PIN_MEMORY = True # page-locked memory, which allows faster and asynchronous transfers from CPU to GPU
+PIN_MEMORY = True # Page-locked memory, which allows faster and asynchronous transfers from CPU to GPU
 LOAD_MODEL = False
 TRAIN_IMG_DIR = "data/train/imgs"
 TRAIN_MASK_DIR = "data/train/labels"
 VAL_IMG_DIR = "data/test/imgs"
-VAL_IMG_DIR = "data/test/imgs"
+VAL_MASK_DIR = "data/test/labels"
 
+# Data preparation
+def data_prep():
+    train_transform = A.Compose([
+        A.Resize(IMAGE_HEIGHT, IMAGE_WIDTH),
+        A.Rotate(limit=30, p=0.5),
+        A.HorizontalFlip(p=0.5),
+        # Single-channel normalization: mean=(0.5,), std=(0.5,) is used to zero-center grayscale images 
+        # ... and scale them to roughly [-1, 1], which improves neural network training stability.
+        A.Normalize(                
+            mean=(0.5,),
+            std=(0.5,),
+        ), 
+        ToTensorV2() # HWC -> CHW, numpy -> torch.Tensor, output dtype: float32
+    ])
+
+    val_transform = A.Compose([
+        A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+        A.Normalize(
+            mean=(0.5,),
+            std=(0.5,),
+        ),
+        ToTensorV2()
+    ])
+
+    train_loader, val_loader = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
+        VAL_IMG_DIR,
+        VAL_MASK_DIR,
+        BATCH_SIZE,
+        train_transform,
+        val_transform,
+        NUM_WORKERS,
+        PIN_MEMORY
+    )
+    
+
+
+# Train loop
+def train():
+    pass
+
+if __name__ == "__main__":
+    train()
---
+++ utils.py
@@ -0,0 +1,36 @@
+import torch
+import torchvision
+from dataset import UNetDataset
+from torch.utils.data import DataLoader
+
+def get_loaders(
+    train_img_dir, 
+    train_mask_dir, 
+    val_img_dir, 
+    val_mask_dir,
+    batch_size, 
+    train_transform, 
+    val_transform, 
+    num_workers,  # data loaded in parallel subprocesses
+    pin_memory=True
+):
+    train_dataset = UNetDataset(image_dir=train_img_dir, mask_dir=train_mask_dir, transform=train_transform)
+    val_dataset = UNetDataset(image_dir=val_img_dir, mask_dir=val_mask_dir, transform=val_transform)
+
+    train_loader = DataLoader(
+        dataset=train_dataset, 
+        batch_size=batch_size, 
+        num_workers=num_workers, 
+        pin_memory=pin_memory, 
+        shuffle=True
+    )
+    val_loader = DataLoader(
+        dataset=val_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers, 
+        pin_memory=pin_memory,
+        shuffle=False
+    )
+
+    return train_loader, val_loader
+

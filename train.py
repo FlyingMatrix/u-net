2a394f01ac9816e702e4cc57a030ac9079afec7a
FlyingMatrix <jxzhang.acm@gmail.com>
2026-01-13 11:02:28 +0100

    create train.py

---
+++ train.py
@@ -0,0 +1,16 @@
+import torch
+import albumentations as A # !pip install albumentations opencv-python
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+from model import UNET
+from utils import (
+    load_checkpoint,
+    save_checkpoint,
+    get_loaders,
+    check_accuracy,
+    save_predictions_as_imgs,
+)
+
+
